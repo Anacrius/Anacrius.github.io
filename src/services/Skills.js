b674231b3c0beb8dba27516e33175aed8aaa788e
UNG Maxime <maxime.ung@epitech.eu>
2020-10-13 09:05:17 -0400

    react render test

---
+++ src/services/Skills.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Skills extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Skills part</h3>
+            </div>
+        );
+    }
+}
+
+export default Skills;
