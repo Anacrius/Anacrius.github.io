420024064b10c6a53a4528ef8528f4d8ca2fad29
UNG Maxime <maxime.ung@epitech.eu>
2020-10-13 08:42:34 -0400

    React render test

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router,
+         Route,
+         Switch, } from 'react-router-dom';
+
+import NavBar from "./jsx/Navbar";
+import Gallery from "./services/Gallery";
+import Skills from "./services/Skills"
+
+const Header = (props) => (
+    <header>
+        <a href="./">
+            <h1 id="head"><span id="job">UNG</span>Maxime</h1>
+        </a>
+        <NavBar />
+    </header>
+);
+
+const Footer = (props) => (
+    <footer>
+        <div className="efooter info">
+            <a className="link" href="./">About</a>
+            <a className="link" href="./">Contact</a>
+        </div>
+    </footer>
+);
+
+const Home = (props) => (
+    <div>Home</div>
+);
+
+export default function App() {
+    return (
+    <Router>
+        <div id="app">
+            <Header />
+            <main>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/gallery" component={Gallery} />
+                    <Route path="/skills" component={Skills} />
+                </Switch>
+            </main>
+            <Footer />
+        </div>
+    </Router>
+    );
+}
