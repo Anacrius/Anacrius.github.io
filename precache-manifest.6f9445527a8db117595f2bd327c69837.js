c705269ae6cdd873928af690e0a80e1c515f852f
UNG Maxime <maxime.ung@epitech.eu>
2020-10-13 08:46:43 -0400

    Updates

---
+++ precache-manifest.6f9445527a8db117595f2bd327c69837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1ab758a0c8ab6220712686b2b298f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f8991a52e53817ad58c",
+    "url": "/static/css/main.79680cbc.chunk.css"
+  },
+  {
+    "revision": "294cc7f805662193d0cc",
+    "url": "/static/js/2.d4fb55d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4fb55d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8991a52e53817ad58c",
+    "url": "/static/js/main.da83779a.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  }
+]);
