1bb4369ddc67074e2c966ca0352a5cdee0f57cf3
UNG Maxime <maxime.ung@epitech.eu>
2021-01-28 05:16:59 -0500

    Updates

---
+++ precache-manifest.584d45cd3e95abd79e93783048ea8869.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c9160b7d3b0ae5cdee479bdf55f9cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa625b2b6261dd622898",
+    "url": "/static/css/main.b206efe7.chunk.css"
+  },
+  {
+    "revision": "c54d2dcf284cd11b1d9e",
+    "url": "/static/js/2.b40b1236.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b40b1236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa625b2b6261dd622898",
+    "url": "/static/js/main.fd9d81d2.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "b5fb2bec15c3f1be5279cbc310739259",
+    "url": "/static/media/Zsh.b5fb2bec.png"
+  },
+  {
+    "revision": "f258cce94623920905a78d2a966d7571",
+    "url": "/static/media/calculator.f258cce9.png"
+  },
+  {
+    "revision": "3f1a7fdef972fdb457f43fac6064da2c",
+    "url": "/static/media/imageComp.3f1a7fde.png"
+  },
+  {
+    "revision": "e33c2680ced2bad0b25cc3f1c466754b",
+    "url": "/static/media/wolfram.e33c2680.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1cf8a7eeedc5af0b4c311042d0f1aab.js"
+  "/precache-manifest.584d45cd3e95abd79e93783048ea8869.js"
 );
 
 self.addEventListener('message', (event) => {
